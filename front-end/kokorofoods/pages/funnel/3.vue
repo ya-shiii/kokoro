aa7741dbc5583d0662921784d2818139173a754b
Michael Earvin Tia <mictt123@gmail.com>
2023-03-29 18:11:53 +0800

    funnel 3 update

--- front-end/kokorofoods/pages/funnel/3.vue
+++ front-end/kokorofoods/pages/funnel/3.vue
@@ -1,12 +1,12 @@
 <template>
-  <main role="main" style="background-color:#F3F5E6;">
+  <main role="main">
     <section class="pl-2 pr-2" v-if="!mobile">
       <div class="mx-auto" style="width: 100% ">
-          <p class="text-uppercase mb-0 py-0 text-center" style="color: black; font-size: 3.5rem; font-weight: bold">
+          <p class="ng text-uppercase mb-0 py-0 text-center">
             <span style="color: #d5441c">24% de descuento</span> En Sal Con Ajo
           </p>
-          <p class="text-uppercase mb-0 py-0 text-center" style="color: black; font-size: 3.5rem; font-weight: bold" >Negro </p>
-          <p class="text-uppercase mt-0 py-0 text-center" style="font-size: 3rem; font-weight: smaller">
+          <p class="ng text-uppercase mb-0 py-0 text-center" >Negro </p>
+          <p class="agr text-uppercase mt-0 py-0 text-center">
             Agregando una pasta de ajo negro
           </p>
       </div>
@@ -20,34 +20,34 @@
       </div>
     </section>
     <section class="py-0" v-if="!mobile">
-      <div class="d-flex justify-content-left" style="margin-left: 15%; margin-top: 30px">
+      <div class="cont d-flex justify-content-left">
         <div>
           <p class="text-start fs-16 mt-5" >
             Disfruta este ajo negro con un recetario que te ayudará a sorprender amigos y familiares.
           </p>
-          <h4 style="font-weight: 700; color: #000000">Qué llevarías:</h4>
+          <h4 class="listed">Qué llevarías:</h4>
           <span><ul style="padding-left: 40px;">
-            <li><span style="font-weight: 700; color: #000000">140gr de Ajo Negro</span> puro $6.500.</li>
-            <li><span style="font-weight: 700; color: #000000">100% Ajo Negro</span> sin conservantes.</li>
-            <li>Producido con Ajo cultivado en <span style="font-weight: 700; color: #000000">Chile.</span></li>
+            <li><span class="listed">140gr de Ajo Negro</span> puro $6.500.</li>
+            <li><span class="listed">100% Ajo Negro</span> sin conservantes.</li>
+            <li>Producido con Ajo cultivado en <span class="listed">Chile.</span></li>
             <li>Listo para usar, no necesita cocción.</li>
-            <li>Recetario <span style="font-weight: 700; color: #000000">Exclusivo</span>  Kokorofoods Valor $3.000</li>
+            <li>Recetario <span class="listed">Exclusivo</span>  Kokorofoods Valor $3.000</li>
           </ul></span>
         </div>
       </div>
         <div class="d-flex flex-column justify-content-center align-items-center">
             <div>
-            <p  class="mt-5 mb-0" style="font-size: 16px;"><del> Antes: $9.500 </del></p>
+            <p  class="p1 mt-5 mb-0"><del> Antes: $9.500 </del></p>
             </div>
             <div>
-            <p class="mb-0" style="font-size: 35px; font-weight: 700;">$23.500</p>
+            <p class="p2 mb-0">$23.500</p>
             </div>
-            <p style="font-size: 16px; font-weight: 700">+ despacho ($4.000)</p>
+            <p class="p3">+ despacho ($4.000)</p>
             <div>
             <button type="button" class="mt-5 mb-0 text-uppercase btn btn-outline-dark">compar</button>
             </div>
             <div class="pb-2 pt-4 mb-5">
-            <a href="" style="color: grey; text-decoration: underline!important; ">No gracias, quiero pagar el despacho de $4.000</a>
+            <a href="" class="btn">No gracias, quiero pagar el despacho de $4.000</a>
             </div>
           </div>
     </section>
@@ -87,11 +87,58 @@ export default {
 }
 main {
   min-height: calc(100vh - 100px); /* Set the minimum height of the main section to be the height of the viewport minus the height of the footer */
+  background-color:#F3F5E6;
 }
 .gray-box {
-  background-color: grey; 
-  height: 429px; 
+  background-color: grey;
+  height: 429px;
   width: 949px;
+}
+
+.ng
+{
+  color: black;
+  font-size: 3.5rem;
+  font-weight: bold;
+}
+.agr
+{
+  font-size: 3rem;
+  font-weight: smaller;
+}
+
+.cont
+{
+  margin-left: 15%;
+  margin-top: 30px;
+}
+
+.lstd
+{
+  font-weight: 700;
+  color: #000000;
+}
+
+.p1
+{
+  font-size: 16px;
+}
+
+.p2
+{
+  font-size: 35px; font-weight: 700;
+}
+
+.p3
+{
+  font-size: 16px;
+  font-weight: 700;
+}
+
+.btn
+{
+color: grey;
+text-decoration: underline!important;
 }
 
 @media only screen and (max-width: 1023px) {
