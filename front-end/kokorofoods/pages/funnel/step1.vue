bda820aef55163012199999bfee22fc446c45d01
jorenlee24 <jo@bluthrive.com>
2023-04-04 12:22:15 +0800

    update

--- front-end/kokorofoods/pages/funnel/step1.vue
+++ front-end/kokorofoods/pages/funnel/step1.vue
@@ -163,16 +163,6 @@ export default {
   auth: "guest",
   data() {
     return {
-      Title: '<span class="text-muted">Ajo negro de calidad,</span> <br>comprometidos con el medio ambiente.',
-            Image_url: '/img/img-nosotros.jpg',
-            Background_color: '#000000',
-            title: '<span class="text-muted">Ajo negro</span><br>Ir al Catálogo ',
-            bu_text: 'Ir al Catálogo',
-            bu_link: '/productos',
-            image_url: '/img/img-catalogo.jpg',
-            image_url_mob: '/img/mobile/img-catalogo-1.jpg',
-            class_section: 'ir-catalogo',
-            title_mobile: 'Headline<span class="text-muted"><br>About Us</span>',
       mobile: false,
     };
   },
